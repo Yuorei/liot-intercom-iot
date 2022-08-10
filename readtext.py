0a60fa5d366dc178995c7a7c97c66a45c1abdae6
tsoushi <sosimds@gmail.com>
2022-08-10 23:12:01 +0900

    main.pyを調整しました。これにより、訪問者の音声の入力がなくても、サーバーからのメッセージを読み上げることができます。さらに、訪問者の音声の入力がなくても一定時間経過すると処理を終了します。

--- readtext.py
+++ readtext.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 import pygame
 import os
-
+import time
 
 def readText(txt):
     tts =gTTS(text=txt,lang="ja")
@@ -12,6 +12,10 @@ def readText(txt):
     pygame.mixer.init()    # 初期設定
     pygame.mixer.music.load(storefile)     # 音楽ファイルの読み込み
     pygame.mixer.music.play(1)              # 音楽の再生回数(1回)
+    # 音声の再生が終了するまで待つ
+    while pygame.mixer.music.get_busy(): # get_busy() は、再生中の場合 True、そうでない場合 False を返す
+        time.time(0.1)
+        
     os.remove(storefile)
 
 if __name__ =="__main__":
