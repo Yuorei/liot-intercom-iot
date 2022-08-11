1498857c4fca2fc85af22b92bd00816e68cbe85e
tsoushi <sosimds@gmail.com>
2022-08-11 17:14:59 +0900

    音声出力を変更

--- readtext.py
+++ readtext.py
@@ -1,21 +1,30 @@
 from gtts import gTTS
-import pygame
+# import pygame
+import pyaudio
+import wave
 import os
 import time
 
 def readText(txt):
     tts =gTTS(text=txt,lang="ja")
 
-    tts.save(r'communication.mp3')
-    storefile ="communication.mp3"
+    tts.save(r'communication.wav')
+    storefile ="communication.wav"
     
-    pygame.mixer.init()    # 初期設定
-    pygame.mixer.music.load(storefile)     # 音楽ファイルの読み込み
-    pygame.mixer.music.play(1)              # 音楽の再生回数(1回)
+    pa = pyaudio.PyAudio()
+    stream = pa.open(44100, 2, pyaudio.paInt16, output=True, output_device_index=1)
+    wav = wave.open(storefile)
+
     # 音声の再生が終了するまで待つ
     print('メッセージを読み上げ中: ', txt)
-    while pygame.mixer.music.get_busy(): # get_busy() は、再生中の場合 True、そうでない場合 False を返す
-        time.sleep(0.1)
+    while 1:
+        data = wav.readframes(wav.getnframes())
+        if not data:
+            break
+        stream.write()
+    stream.close()
+    wav.close()
+    pa.terminate()
     print('メッセージの読み上げ完了')
     os.remove(storefile)
 
